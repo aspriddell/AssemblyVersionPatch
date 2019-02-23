4822eba66f3c1b1fbfa61f20263e401604c20ea5
Albie <albion@dragonfruit.ml>
2019-02-23 19:54:29 +0000

    First push

---
+++ AssemblyVersionPatch/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyVersionPatch
+{
+    class Program
+    {
+        static List<string> newfile = new List<string>();
+        static string file_location;
+        static readonly string linetowrite = $"[assembly: AssemblyVersion(\"{DateTime.Now:yyyy.ddMM.HH}\")]";
+
+        static void Main(string[] args)
+        {
+            try { file_location = args[0]; }
+            catch { return; }
+
+            string[] file = File.ReadAllLines(file_location);
+            foreach (var line in file)
+            {
+                if (line.StartsWith("[assembly: AssemblyVersion"))
+                    newfile.Add(linetowrite);
+                else
+                    newfile.Add(line);
+            }
+
+            File.WriteAllLines(file_location, newfile.ToArray());
+        }
+    }
+}
---
+++ AssemblyVersionPatch/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Assembly Version Patcher")]
+[assembly: AssemblyCompany("Albie Spriddell")]
+[assembly: AssemblyProduct("AssemblyVersionPatch")]
+[assembly: AssemblyCopyright("Copyright © Albie Spriddell 2019")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("964ca6f5-596b-44cf-9070-8a6af21c0d91")]
+
+[assembly: AssemblyVersion("2019.2302.19")]
+[assembly: AssemblyFileVersion("1")]
